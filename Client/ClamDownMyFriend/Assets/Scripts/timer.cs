c6da8c849d094e7a66fe26273687eca510a4bd94
newlusifer <46846655+newlusifer@users.noreply.github.com>
2020-05-05 13:08:18 +0700

    add_Winner

---
+++ Client/ClamDownMyFriend/Assets/Scripts/timer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    private float time = 0;
+    public Text minute;
+    public Text sec;
+
+    private int m = 0;
+    private int s = 0;
+
+    private string statusClock = "";
+
+    public static string minuteShare = "";
+    public static string secondShare ="";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Status Game Is.... "+ ConnectionManager.statusGame);
+
+        statusClock=ConnectionManager.statusGame;
+        Debug.Log("Status clock Is.... " + statusClock);
+
+        if (statusClock == "start")
+        {
+           
+            time += Time.deltaTime;
+
+            if (time >= 60)
+            {
+                m++;
+                time = 0;
+            }
+
+            minute.text = m.ToString();
+            s = Mathf.RoundToInt(time);
+            sec.text = s.ToString();
+
+            minuteShare = m.ToString();
+            secondShare = s.ToString();
+        }
+
+        if (statusClock == "restart")
+        {
+            time = 0;
+            m = 0;
+            s = 0;
+}
+
+
+
+
+        }
+}
---
+++ Server/ClamDownMyFriend_Server/server.js
@@ -0,0 +1,294 @@
+var io = require("socket.io")(5055);
+var uid = require('uid');
+
+console.log("socket listen at port 5055");
+
+var playerIDList = [];
+
+var roomIDList = [];
+var roomDataList = {};
+
+var randomMap = Math.floor((Math.random() * 9)+1);
+
+io.on("connection", (socket)=>{
+
+    console.log("client connected : "+socket.id);
+
+    //socket.join("lobby"+uid());
+
+    ClientConnect(io, socket);
+
+    ClientFetchPlayerList(socket);
+
+    ClientCreateRoom(socket);
+
+    ClientJoinRoom(socket);
+
+    ClientLeaveRoom(socket);
+
+    ClientFetchRoomList(socket);
+
+    ClientUpdateMove(socket);
+
+    socket.on('OnPlayerWinner', function (data) {
+
+        randomMap = Math.floor((Math.random() * 9)+1);
+
+        var dataSend = {
+            "playerName":data.playername,"timeM":data.timeM,"timeS":data.timeS,"randomMap":randomMap
+            
+        };
+        
+        socket.broadcast.to(data.currentroomname).emit("OnWinnerShow", dataSend);
+        socket.emit("OnWinnerShow", dataSend);
+        console.log("New Map Is"+randomMap);
+        
+         console.log('Winner is '+data.playername+' with time '+data.timeM+' : '+data.timeS+' On Room Name '+data.currentroomname);
+    });
+
+    socket.on("disconnect", ()=>{
+
+        console.log("client disconnected : "+socket.id);
+
+        ClientDisconnect(io, socket);
+    });
+});
+
+setInterval(()=>{
+
+    for(var i = 0; i < roomIDList.length; i++)
+    {
+        var roomName = roomIDList[i];
+        for(var j = 0; j < roomDataList[roomName].sockets.length; j++)
+        {
+            var socket = roomDataList[roomName].sockets[j];
+
+            if(socket != undefined)
+            {
+                socket.emit("OnClientUpdateMoveList", roomDataList[roomName].playerData);
+            }
+        }
+    }
+
+}, 100);
+
+var ClientConnect = (io, socket)=>{
+
+    var data = {
+        "uid":socket.id
+    };
+
+    CountPlayer();
+
+    socket.emit("OnOwnerClientConnect", data);
+
+    console.log(randomMap);
+}
+
+var ClientDisconnect = (io, socket)=>{
+
+    var data = {
+        "uid":socket.id
+    };
+
+    /*for(var i = 0; i < playerIDList.length; i++)
+    {
+        if(playerIDList[i] == data.uid)
+        {
+            playerIDList.splice(i, 1);
+            console.log("delete player : " + data.uid);
+        }
+    }*/
+
+    LeaveRoom(socket);
+
+    CountPlayer();
+
+    io.emit("OnClientDisconnect", data);
+}
+
+var ClientFetchPlayerList = (socket)=>{
+
+    socket.on("OnClientFetchPlayerList", (data)=>{
+
+        var isRoomFound = (element)=>{
+            return element == data.roomName;
+        }
+
+        var roomName = data.roomName;
+
+        if(roomIDList.find(isRoomFound) != undefined)
+        {
+            var playerIds = [];
+
+            for(var i = 0; i < roomDataList[roomName].sockets.length; i++)
+            {
+                playerIds.push(roomDataList[roomName].sockets[i].id);
+            }
+
+            var data = {
+                "playerIDList": playerIds,
+            }
+            socket.emit("OnClientFetchPlayerList", data);
+        }
+        else
+        {
+            var data = {};
+            socket.emit("OnClientFetchPlayerList", data);
+        }
+    });
+} 
+
+var CountPlayer = ()=>{
+    console.log("Player total : "+ playerIDList.length);
+}
+
+
+var ClientFetchRoomList = (socket)=>{
+    
+    socket.on("OnClientFetchRoomList", ()=>{
+
+        data = {
+            "roomIDList":roomIDList
+        }
+
+        socket.emit("OnClientFetchRoomList", data);
+    });
+}
+
+var ClientCreateRoom = (socket)=>{
+    //roomName : string
+    socket.on("OnClientCreateRoom", data=>{
+        console.log("ClientCreateRoom : "+socket.id +" : "+data.roomName);
+
+        var isRoomFound = (element)=>{
+            return element == data.roomName;
+        }
+
+        if(roomIDList.find(isRoomFound) != undefined || data.roomName == ""){
+            console.log("ClientCreateRoom : fail");
+            socket.emit("OnClientCreateRoomFail", data);
+        }else{
+            roomIDList.push(data.roomName);
+            roomDataList[data.roomName] = {
+                "sockets": [socket],
+                "playerData": {},
+            };
+
+            var resultData = {
+                uid: socket.id
+            };
+
+
+            socket.join(data.roomName, ()=>{
+                console.log("CientCreatRoom : success");
+                socket.emit("OnClientCreateRoomSuccess", resultData);
+
+                var data = {
+                    "startgame":"start","randomMap":randomMap
+                };
+
+                socket.emit("OnStartGame",data);
+
+            });
+        }
+        
+    });
+}
+
+var ClientJoinRoom = (socket)=>{
+    socket.on("OnClientJoinRoom", (data)=>{
+        console.log("ClientJoinRoom");
+
+        var isRoomFound = (element)=>{
+            return element == data.roomName;
+        };
+
+        if(roomIDList.find(isRoomFound) != undefined && data.roomName != ""){
+            
+           
+            roomDataList[data.roomName].sockets.push(socket);
+
+            var resultData = {
+                uid: socket.id
+            };
+
+            socket.join(data.roomName, ()=>{
+                console.log("ClientJoinRoom : success");
+                socket.emit("OnOwnerClientJoinRoomSuccess", resultData);
+                socket.broadcast.to(data.roomName).emit("OnClientJoinRoomSuccess", resultData);
+
+                var data = {
+                    "startgame":"start","randomMap":randomMap
+                };
+
+                socket.emit("OnStartGame",data);
+            
+            });
+
+        }else{
+            console.log("ClientJoinRoom : fail");
+            socket.emit("OnClientJoinRoomFail", data);
+        }
+    });
+}
+
+var ClientLeaveRoom = (socket)=>{
+
+    socket.on("OnClientLeaveRoom", ()=>{
+        console.log("client leave room");
+        LeaveRoom(socket);
+    });
+}
+
+var LeaveRoom = (socket)=>{
+
+    for(var i = 0; i < roomIDList.length;i++)
+    {
+        var roomName = roomIDList[i];
+
+        for(var j = 0; j < roomDataList[roomName].sockets.length; j++)
+        {
+            if(roomDataList[roomName].sockets[j].id == socket.id)
+            {
+                roomDataList[roomName].sockets.splice(j,1);
+                
+                delete roomDataList[roomName].playerData[socket.id];
+
+                var data = {
+                    uid: socket.id
+                };
+
+                socket.broadcast.to(roomName).emit("OnClientLeaveRoom", data);
+                socket.emit("OnClientLeaveRoom", data);
+                socket.leave(roomName);
+
+                break;
+            }
+        }
+
+        if(roomDataList[roomName].sockets.length <= 0)
+        {
+            roomIDList.splice(i, 1);
+            delete roomDataList[roomName];
+            break;
+        }
+    }
+}
+
+var ClientUpdateMove = (socket)=>{
+
+    socket.on("OnClientUpdateMove", (data)=>{
+
+        if(roomDataList[data.roomName] != undefined)
+        {
+            roomDataList[data.roomName].playerData[data.uid] = {
+                x: data.x,
+                y: data.y,
+                z: data.z,
+            };
+        }
+        
+
+    });
+}
